9f322bb702e48ae1455ed74076c8edf38ac0aa01
Ch. Michalakis <mpampisb@gmail.com>
2025-09-25 18:00:15 +0300

    chore: add id attribute to section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import ParallaxBackground from "../components/ParallaxBackground.jsx";
 const Hero = () => {
 
     return (
-        <section className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+        <section id="hero" className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
             <HeroText />
             <ParallaxBackground />
         </section>
