8f5ef516186dc351fca9f21ac2dc584cf0552807
Ch. Michalakis <mpampisb@gmail.com>
2025-09-29 02:54:32 +0300

    feat: complete Projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -22,13 +22,13 @@ const Projects = () => {
                     </div>
                     <div className="text-center m-auto mb-20 w-[48rem] p-5 bg-gradient-to-b from-reservabg to-red-300 dark:text-navy special-shadow" >
                         <h2 className="text-2xl font-black mb-2 text-gray-200">Project Description</h2>
-                        "myReserva" is a useful app that handles appointments and reservations for small businesses. It is
-                        my greatest project so far. Its highlight; It works with two completely different backend technologies.
-                        A Node.js and a Spring Boot; both exposing a RESTful API that the frontend makes calls to. The frontend
-                        was implemented using React + Vite + TypeScript as well as a variety of other technologies such as zod
-                        validation, Flowbite components, React Router and more. Both frontend and backend provide robust authentication
-                        handling, validation and clean architecture.
+                        <strong>myReserva</strong> is an application designed to manage appointments and reservations for small businesses.
+                        It stands out as my most significant project so far, with a unique architecture that integrates two distinct backend technologies:
+                        Node.js and Spring Boot, both exposing RESTful APIs consumed by the frontend. <br/>
 
+                        The frontend is built with React, Vite, and TypeScript, and leverages modern tools such as Zod for validation,
+                        Flowbite for UI components, and React Router for navigation. Both the frontend and backend provide robust authentication, data validation,
+                        and a clean architecture, ensuring reliability and scalability.
                     </div>
                 </div>
             )}
@@ -44,11 +44,11 @@ const Projects = () => {
                     >
                         <ThreeDCard
                             imgSrc="src/public/assets/projects_screenshots/myreserva.png"
-                            title="School App - Spring Boot API"
-                            description="A RESTful api providing CRUD operations for teacher-users with JWT authentication"
+                            title="myReserva Full-Stack App"
+                            description="A full stack application, handling appointments/reservations for small businesses. Works with two completely different backend technologies."
                         >
                             <GitHubLinkComponent
-                                url="https://github.com/CharioMich/spring-boot-school-app-api"
+                                url="https://github.com/CharioMich/myReserva-app"
                             />
                         </ThreeDCard>
                     </motion.div>
@@ -82,6 +82,22 @@ const Projects = () => {
                     >
                         <GitHubLinkComponent
                             url="https://github.com/CharioMich/spring-boot-school-app-api"
+                        />
+                    </ThreeDCard>
+                </motion.div>
+                <motion.div
+                    initial={{opacity: 0, y: 100}}
+                    whileInView={{opacity: 1, y: 0}}
+                    viewport={{amount: 0.1}}
+                    transition={{duration: 1}}
+                >
+                    <ThreeDCard
+                        imgSrc="src/public/assets/projects_screenshots/springreserva.webp"
+                        title="Spring Boot API"
+                        description="Spring Boot api for myReserva app, providing robust authentication and security."
+                    >
+                        <GitHubLinkComponent
+                            url="https://github.com/CharioMich/my-reserva-spring-api"
                         />
                     </ThreeDCard>
                 </motion.div>
