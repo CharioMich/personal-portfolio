c300d0b7d2cf30b57547e72d6a42bc23c3dfea9b
Ch. Michalakis <mpampisb@gmail.com>
2025-10-03 02:18:55 +0300

    feat!: change layout and 3D model movement

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -2,23 +2,27 @@
 import Phone from "@/components/Phone.jsx";
 import Loader from "@/components/Loader.jsx";
 
-import {Suspense, useState} from "react";
+import {Suspense, useEffect, useState} from "react";
 import { Center, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { a, useSpring } from '@react-spring/three';
+import {useMediaQuery} from "react-responsive";
 
 import { motion } from "motion/react";
 import PhoneCard from "@/components/PhoneCard.jsx";
 import {StarsBackground} from "@/components/StarsBackground.jsx";
 
+import {Pointer} from "lucide-react";
 
 const Contact = () => {
+
+    const isDesktop = useMediaQuery({minWidth: 769})
 
     const [isInside, setIsInside] = useState(false);
 
     // Define the initial state (which will also be the 'reset' state)
-    const initialPosition = [13, 0, 2];
-    const initialRotation = [0.3, -0.65, 0];
+    const initialPosition = isDesktop ? [18, 0, 4] : [0, 0, 0];
+    const initialRotation = [0.32, -0.69, 0];
 
     // Use the useSpring hook to manage the animated properties
     const [springs, api] = useSpring(() => ({
@@ -34,25 +38,17 @@ const Contact = () => {
     const targetPosition = [1, -4, 40];
     const targetRotation = [0.9, 0.75, 0];
 
+    useEffect(() => {
+        if (isInside) {
+            api.start({ position: targetPosition, rotation: targetRotation });
+        } else {
+            api.start({ position: initialPosition, rotation: initialRotation });
+        }
+    }, [isInside]);
 
-    const handleMovement = () => {
-        // Use api.start method to trigger smooth transition
-        api.start({
-            position: targetPosition,
-            rotation: targetRotation
-        });
+    const handleMovement = () => setIsInside(true);
+    const handleReset = () => setIsInside(false);
 
-        setIsInside(true);
-    }
-
-    const handleReset = () => {
-        api.start({
-            position: initialPosition,
-            rotation: initialRotation
-        });
-
-        setIsInside(false);
-    }
 
     const cardVariants = {
         // This is the state when the card IS visible (isInside is true)
@@ -89,20 +85,36 @@ const Contact = () => {
                 {/*>*/}
                 <StarsBackground/>
                     {/* Outer div */}
-                    <div className="flex flex-col items-center h-full justify-around md:flex-row z-30"
-                        onMouseEnter={handleMovement}
+                    <div className="flex flex-col items-center h-full justify-between md:flex-row z-30"
                         onMouseLeave={handleReset}
                     >
 
                         {/* Text */}
                         <div
-                            className="flex flex-col w-full md:flex-row z-30 items-center justify-around"
+                            className={`flex flex-col w-full md:flex-row z-30 md:mb-10 items-center justify-around transition-colors duration-1500 ${isInside ? "text-white" : ""}`}
                         >
-                            <div className="p-4 light:triple-shadow rounded-xl bg-gray-200/70 text-black dark:text-white dark:bg-transparent">
-                                <p className="text-xl dark:text-white font-mono p-2 w-fit">Thank you for coming this far!</p>
-                                <h1 className="text-6xl font-extrabold">
-                                    Let's get in touch!
-                                </h1>
+                            <div className="flex flex-col items-center gap-4 font-bold">
+                                {/*<p className="text-2xl dark:text-white font-mono p-2 w-fit">Thank you for coming this far!</p>*/}
+                                <div
+                                    className="flex flex-col text-2xl font-bold items-center gap-6 w-fit"
+                                >
+                                    <p>Check out my GitHub profile!</p>
+                                    <a
+                                        href="https://github.com/CharioMich"
+                                        target="_blank">
+                                        <img
+                                            className="rounded-full shadow-lg shadow-gray-600 hover:transition transition-all duration-500 ease-in-out hover:shadow-lg hover:shadow-emerald-600 hover:scale-110"
+                                            src="/assets/ghicon.png"
+                                            alt="Github Icon"/>
+                                    </a>
+                                </div>
+                                <p>or..</p>
+                                <div className="flex flex-col w-fit items-center mb-2 md:gap-4 cursor-pointer hover:transition-all duration-700 ease-in-out
+                                hover:text-shadow-lg hover:text-shadow-fuchsia-700/70 hover:scale-105" onClick={handleMovement}>
+                                    <h1 className="text-6xl text-center font-extrabold mt-2">Let's get in touch!</h1>
+                                    <Pointer size={50} className={`mt-1 animate-pulse transition-all duration-500 ${isInside ? "invisible" : ""}`} />
+                                </div>
+
                             </div>
                             {/*PhoneCard*/}
                             <motion.div
@@ -110,14 +122,13 @@ const Contact = () => {
                                 animate={isInside ? "visible" : "hidden"}
                                 variants={cardVariants}
                                 className={`z-30 max-w-md max-h-md motion-element ${!isInside ? "pointer-events-none" : ""}`}
-                                onMouseEnter={handleMovement}
                             >
                                 <PhoneCard/>
                             </motion.div>
                         </div>
                         {/* 3D Model */}
-                        <div className="absolute inset-0 z-20 m-auto" onMouseLeave={handleReset}>
-                            <Canvas onMouseLeave={handleReset}>
+                        <div className="absolute inset-0 z-20 m-auto">
+                            <Canvas>
                                 <PerspectiveCamera makeDefault position={[0, 0, 50]} />
                                 <Suspense fallback={<Loader />}>
                                     <ambientLight intensity={0.5} color={"#00ffcc"} />
